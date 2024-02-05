06b592688ea58163a6134766b6a404ce299a32a0
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2024-02-04 23:05:04 -0600

    First Commit
    Se descargaron los archivos con los cuales se estaran trabajando.

---
+++ app.js
@@ -0,0 +1,6 @@
+//seleccinoamos algunos tags del HTML para cambiar su contenido de forma dinamica
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Juego del numero secreto";
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML = "Escribe un numero del 1 al 10";
